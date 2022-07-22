49787d68b71e9c9dd0622b26cf3d8d781ae96c1e
Timo Müller <timo.mueller92@outlook.de>
2022-07-22 16:23:32 +0200

    Übung 1 bearbeitet

--- Übungsaufgaben/Form1.Designer.cs
+++ Übungsaufgaben/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Übungsaufgaben
+﻿namespace Übung_1
 {
     partial class Form1
     {
@@ -28,12 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtInput1 = new System.Windows.Forms.TextBox();
+            this.ddn = new System.Windows.Forms.ComboBox();
+            this.txtInput2 = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.bntCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput1
+            // 
+            this.txtInput1.Location = new System.Drawing.Point(12, 12);
+            this.txtInput1.Name = "txtInput1";
+            this.txtInput1.Size = new System.Drawing.Size(100, 23);
+            this.txtInput1.TabIndex = 0;
+            // 
+            // ddn
+            // 
+            this.ddn.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ddn.FormattingEnabled = true;
+            this.ddn.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.ddn.Location = new System.Drawing.Point(118, 12);
+            this.ddn.Name = "ddn";
+            this.ddn.Size = new System.Drawing.Size(35, 23);
+            this.ddn.TabIndex = 1;
+            // 
+            // txtInput2
+            // 
+            this.txtInput2.Location = new System.Drawing.Point(159, 12);
+            this.txtInput2.Name = "txtInput2";
+            this.txtInput2.Size = new System.Drawing.Size(100, 23);
+            this.txtInput2.TabIndex = 2;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(12, 38);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(32, 15);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "lable";
+            this.lblResult.Visible = false;
+            // 
+            // bntCalculate
+            // 
+            this.bntCalculate.Location = new System.Drawing.Point(265, 12);
+            this.bntCalculate.Name = "bntCalculate";
+            this.bntCalculate.Size = new System.Drawing.Size(75, 23);
+            this.bntCalculate.TabIndex = 4;
+            this.bntCalculate.Text = "Berechnen";
+            this.bntCalculate.UseVisualStyleBackColor = true;
+            this.bntCalculate.Click += new System.EventHandler(this.BntCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(354, 61);
+            this.Controls.Add(this.bntCalculate);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtInput2);
+            this.Controls.Add(this.ddn);
+            this.Controls.Add(this.txtInput1);
+            this.Name = "Form1";
+            this.Text = " Übung 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox txtInput1;
+        private ComboBox ddn;
+        private TextBox txtInput2;
+        private Label lblResult;
+        private Button bntCalculate;
     }
 }
--- Übungsaufgaben/Form1.cs
+++ Übungsaufgaben/Form1.cs
@@ -1,10 +1,39 @@
-namespace Übungsaufgaben
+namespace Übung_1;
+
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    public Form1()
     {
-        public Form1()
+        InitializeComponent();
+    }
+
+    private void BntCalculate_Click(object sender, EventArgs error)
+    {
+        decimal number1;
+        decimal number2;
+        decimal result; 
+
+        try {
+            number1 = Decimal.Parse(txtInput1.Text);
+            number2 = Decimal.Parse(txtInput2.Text);
+
+            result = ddn.Text switch
+            {
+                "+" => number1 + number2,
+                "-" => number1 - number2,
+                "*" => number1 * number2,
+                "/" => number1 / number2,
+                _ => throw new ArgumentException()
+            };
+
+            lblResult.Text = "Ergebnis: " + result;
+            lblResult.Visible = true;
+        }
+        catch (Exception ex)
         {
-            InitializeComponent();
+            // TODO: Open new window
+            lblResult.Text = ex.Message;
+            lblResult.Visible = true;
         }
     }
 }
